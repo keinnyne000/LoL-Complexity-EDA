a47bed31e7c39c96955b7418d032477dfcb6da6b
Ryan Stevens <keinnyne@gmail.com>
2025-04-16 00:29:15 -0600

    add timeline collection for match collector

--- match_collector.py
+++ match_collector.py
@@ -12,7 +12,7 @@ import sys
 from helpers import write_json_list, parse_match_data
 
 ACCESS_POINT = "https://americas.api.riotgames.com"
-API_KEY = "RGAPI-a264d8c4-2502-4f2a-a234-95e58fe8b903"
+API_KEY = "RGAPI-fa5c0ab7-2bf3-46b1-88e3-c6ce0355afe6"
 
 RATE_LIMIT = 100
 WINDOW = 120  # seconds
@@ -85,12 +85,19 @@ async def async_get_match_data(session, match_id, api_key):
     url = f"{ACCESS_POINT}/lol/match/v5/matches/{match_id}?api_key={api_key}"
     return await limited_get(session, url)
 
-async def handle_player(session, player, api_key, depth):
-    puuid_short = player['puuid'][:8]
+async def async_get_timeline_data(session, match_id, api_key):
+    url = f"{ACCESS_POINT}/lol/match/v5/matches/{match_id}/timeline?api_key={api_key}"
+    return await limited_get(session, url)
+
+#----------------------------------------------------------------------------------------------------
+
+# Gets match datas for a player with a given depth
+async def get_player_match_data(session, player_id, api_key, depth):
+    puuid_short = player_id['puuid'][:8]
     print(f"[DEBUG] Handling player {puuid_short}...")
     try:
         print(f"[INFO] Fetching match IDs for player {puuid_short}")
-        match_ids = await async_get_matches(session, player['puuid'], api_key, count=depth)
+        match_ids = await async_get_matches(session, player_id['puuid'], api_key, count=depth)
         print(f"[INFO] Retrieved {len(match_ids)} matches for {puuid_short}")
         print(f"[INFO] Fetching match data for {puuid_short}...")
         tasks = [async_get_match_data(session, match_id, api_key) for match_id in match_ids]
@@ -101,27 +108,27 @@ async def handle_player(session, player, api_key, depth):
         print(f"[ERROR] Failed to process player {puuid_short}: {e}")
         return []
 
-async def get_division_matches(tier, division, player_count, depth, api_key):
-    start = time.time()
-    print(f"[INFO] Starting async_get_league_data | Tier: {tier}, Division: {division}, Player count: {player_count}, Depth: {depth}")
-    start_time = time.time()
+# Gets timeline data for a player with a given depth
+async def get_player_timeline_data(session, player_data, api_key, depth):
+    puuid_short = player_data['puuid'][:8]
+    print(f"[DEBUG] Getting timeline data for {puuid_short}...")
+    try:
+        print(f"[INFO] Fetching match IDs for player {puuid_short}")
+        match_ids = await async_get_matches(session, player_data['puuid'], api_key, count=depth)
+        print(f"[INFO] Retrieved {len(match_ids)} matches for {puuid_short}")
+        print(f"[INFO] Fetching timeline data for {puuid_short}...")
+        tasks = [async_get_timeline_data(session, match_id, api_key) for match_id in match_ids]
+        results = await asyncio.gather(*tasks)
+        print(f"[INFO] Parsing timeline data for {puuid_short}")
+        return [parse_match_data(timeline) for timeline in results]
+    except Exception as e:
+        print(f"[ERROR] Failed to process player {puuid_short}: {e}")
+        return []
 
-    async with aiohttp.ClientSession() as session:
-        print("[INFO] Fetching league page data...")
-        results = []
-        page_number = 1
-        while(len(results) < player_count):
-            print(f"[INFO] Fetching league page data no.{len(results)}...")
-            page_data = await async_get_league_page(session, tier, division, page_number, api_key)
-            results.extend(page_data)
-            page_number += 1
-        players = results[:player_count]
-        print(f"[INFO] Retrieved {len(players)} players from league page.")
 
-        print("[INFO] Processing all players asynchronously...")
-        tasks = [handle_player(session, player, api_key, depth) for player in players]
-        results = await tqdm_asyncio.gather(*tasks, desc="Processing Players")
+#----------------------------------------------------------------------------------------------------
 
+def process_data(results, start_time):
         match_data = []
         for result in results:
             match_data.extend(result)
@@ -131,69 +138,157 @@ async def get_division_matches(tier, division, player_count, depth, api_key):
         
         return match_data
 
+#-------------------------------------------------------------------------------------
+
+# returns a list of player ids on the given tier and division
+async def get_players_by_division(session, tier, division, player_count, api_key):
+    results = []
+    page_number = 1
+    print(f"[INFO] Sampling {player_count} players from {tier} {division}...")
+    while(len(results) < player_count):
+        print(f"[INFO] Fetching league page data no.{len(results)} for {tier} {division}...")
+        page_data = await async_get_league_page(session, tier, division, page_number, api_key)
+        results.extend(page_data)
+        page_number += 1
+    return results[:player_count]
+
+async def get_players_from_all_divisions(session, player_count, api_key):
+    results = []
+    for tier in ['IRON', 'BRONZE', 'SILVER', 'GOLD', 'PLATINUM', 'DIAMOND']:
+        for division in ['I', 'II', 'III', 'IV']:
+            players = await get_players_by_division(session, tier, division, player_count, api_key)
+            results.extend(players)
+    for tier in ['MASTER', 'GRANDMASTER', 'CHALLENGER']:
+        players = await get_players_by_division(session, tier, 'I', player_count, api_key)
+        results.extend(players)
+    return results
+
+# returns a list of player ids based on the sampled distribution
+async def get_players_distribution(session, distribution, player_count, api_key):
+    players = []
+    for d in distribution.iloc():
+        count = int(np.ceil(d['percentage'] * player_count))
+        players.extend(await get_players_by_division(session, d['tier'], d['division'], count, api_key))
+    return players
+
+#----------------------------------------------------------------------------------------
+
+# returns a list of match data for the given players
+async def get_match_data_from_players(session, players, api_key, depth):
+    tasks = [get_player_match_data(session, player, api_key, depth) for player in players]
+    results = await tqdm_asyncio.gather(*tasks, desc="Processing Players")
+    return results
+
+async def get_timeline_data_from_players(session, players, api_key, depth):
+    tasks = [get_player_timeline_data(session, player, api_key, depth) for player in players]
+    results = await tqdm_asyncio.gather(*tasks, desc="Processing Players")
+    return results
 
 #----------------------------------------------------------------------------------------------------
 
-async def get_distribution_matches(player_count: int, depth: int, distribution, api_key):
-    start = time.time()
+
+async def get_timelines_from_all_divisions(session, player_count, depth, api_key):
+    print(f"[INFO] Starting async_get_timeline_data | Player Count: {player_count}, Depth: {depth}")
+    start_time = time.time()
+    players = await get_players_from_all_divisions(session, player_count, api_key)
+    results = await get_timeline_data_from_players(session, players, api_key, depth)
+    return process_data(results, start_time)
+
+async def get_timelines_from_division(session, tier, division, player_count, depth, api_key):
+    print(f"[INFO] Starting async_get_timeline_data | Tier: {tier}, Division: {division}, Player count: {player_count}, Depth: {depth}")
+    start_time = time.time()
+    players = await get_players_by_division(session, tier, division, player_count, api_key)
+    results = await get_timeline_data_from_players(session, players, api_key, depth)
+    return process_data(results, start_time)
+
+async def get_timelines_from_distribution(session, player_count, depth, distribution, api_key):
+    print(f"[INFO] Starting async_get_timeline_data | Player Count: {player_count}, Depth: {depth}")
+    start_time = time.time()
+    players = await get_players_distribution(session, distribution, player_count, api_key)
+    results = await get_timeline_data_from_players(session, players, api_key, depth)
+    return process_data(results, start_time)
+
+async def get_matches_from_all_divisions(session, player_count, depth, api_key):
+    print(f"[INFO] Starting async_get_league_data | Player Count: {player_count}, Depth: {depth}")
+    start_time = time.time()
+    players = await get_players_from_all_divisions(session, player_count, api_key)
+    results = await get_match_data_from_players(session, players, api_key, depth)
+    return process_data(results, start_time)
+
+# returns a list of match data for the given division
+async def get_matches_from_division(session, tier, division, player_count, depth, api_key):
+    print(f"[INFO] Starting async_get_league_data | Tier: {tier}, Division: {division}, Player count: {player_count}, Depth: {depth}")
+    start_time = time.time()
+    players = await get_players_by_division(session, tier, division, player_count, api_key)
+    results = await get_match_data_from_players(session, players, api_key, depth)
+    return await process_data(results, start_time)
+
+async def get_matches_from_distribution(session, player_count: int, depth: int, distribution, api_key):
     print(f"[INFO] Starting async_sample_distribution | Player Count: {player_count}, Depth: {depth}")
     start_time = time.time()
-    
-    async with aiohttp.ClientSession() as session:
-        players = []
-        for d in distribution.iloc():
-            count = int(np.ceil(d['percentage'] * player_count))
-            results = []
-            print(f"[INFO] Sampling {count} players from {d['tier']} {d['division']}...")
-            page_number = 1
-            while(len(results) < count):
-                print(f"[INFO] Fetching league page data no.{len(results)} for {d['tier']} {d['division']}...")
-                page_data = await async_get_league_page(session, d['tier'], d['division'], page_number, api_key)
-                results.extend(page_data)
-                page_number += 1
-            players.extend(results[:count])
-            print(f"[INFO] Retrieved {len(results)} players from for {d['tier']} {d['division']}.")
+    players = await get_players_distribution(session, distribution, player_count, api_key)
+    results = await get_match_data_from_players(session, players, api_key, depth)
+    return await process_data(results, start_time)
 
-        print("[INFO] Processing all players asynchronously...")
-        tasks = [handle_player(session, player, api_key, depth) for player in players]
-        results = await tqdm_asyncio.gather(*tasks, desc="Processing Players")
+#-----------------------------------------------------------------------------------------------------
 
-        match_data = []
-        for result in results:
-            match_data.extend(result)
+def parse_common_args(args):
+    if len(args) < 3:
+        raise ValueError(f"Expected at least {3} arguments, got {len(args)}")
+    file_path = args[0]
+    try:
+        count = int(args[1])
+        depth = int(args[2])
+    except ValueError:
+        raise ValueError("count and depth must be integers")
+    print(f"[INFO] File path: {file_path}, Count: {count}, Depth: {depth}")
+    return file_path, count, depth
 
-        total_time = time.time() - start_time
-        print(f"[INFO] Finished processing all data in {total_time:.2f} seconds. Total matches: {len(match_data)}")
-        print(f"[INFO] Sampled from distribution with depth {depth} and player count {player_count}.")
+async def main(args):
+    flag = args[-1]
 
-        return match_data
-
-
-#------------------------------------------------------------------------------------
-
-async def main(file_path = "test.json", count = 100, depth = 1, flag = 0):
-    if flag == '-s':
-        print("[INFO] Fetching distribution data...")
+    if flag == '-ta':
+        print("[INFO] Fetching timeline data for all divisions...")
+        file_path, count, depth = parse_common_args(args[1:])
+        async with aiohttp.ClientSession() as session:
+            result = await get_timelines_from_all_divisions(session, count, depth, API_KEY)
+            write_json_list(result, file_path)
+    elif flag == '-tv':
+        print("[INFO] Fetching timeline data for a specific division...")
+        file_path, count, depth = parse_common_args(args[1:])
+        tier, division = args[3:5]
+        async with aiohttp.ClientSession() as session:
+            result = await get_timelines_from_division(session, tier, division, count, depth, API_KEY)
+            write_json_list(result, file_path)
+    elif flag == '-ts':
+        print("[INFO] Fetching timeline data by distribution...")
+        file_path, count, depth = parse_common_args(args[1:])
         distribution = pd.read_json('rank_distribution_32825.json')
-        result = await get_distribution_matches(count, depth, distribution, API_KEY)
-        write_json_list(result, file_path)
-    
-    elif flag == '-v':
-        print("[INFO] Fetching division data...")
-        result = await get_division_matches(tier = 'DIAMOND', 
-                                             division = "II", 
-                                             player_count = count,
-                                             depth = depth, 
-                                             api_key = API_KEY)
-        write_json_list(result, file_path)
+        async with aiohttp.ClientSession() as session:
+            result = await get_timelines_from_distribution(session, count, depth, distribution, API_KEY)
+            write_json_list(result, file_path)
+    elif flag == '-ma':
+        print("[INFO] Fetching match data for all divisions...")
+        file_path, count, depth = parse_common_args(args[1:])
+        async with aiohttp.ClientSession() as session:
+            result = await get_matches_from_all_divisions(session, count, depth, API_KEY)
+            write_json_list(result, file_path)
+    elif flag == '-mv':
+        print("[INFO] Fetching match data for a specific division...")
+        file_path, count, depth = parse_common_args(args[1:])
+        tier, division = args[3:5]
+        async with aiohttp.ClientSession() as session:
+            result = await get_matches_from_division(session, tier, division, count, depth, API_KEY)
+            write_json_list(result, file_path)
+    elif flag == '-ms':
+        print("[INFO] Fetching match data by distribution...")
+        file_path, count, depth = parse_common_args(args[1:])
+        async with aiohttp.ClientSession() as session:
+            distribution = pd.read_json('rank_distribution_32825.json')
+            result = await get_matches_from_distribution(session, count, depth, distribution, API_KEY)
+            write_json_list(result, file_path)
+    else:
+        raise ValueError(f"Unknown flag: {flag}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python match_collector.py <output_path> <count> <depth> <flag>")
-        #flags: 0: division_matches, 1: distribution_matches
-        sys.exit(1)
-    
-    output_path, count, depth, flag = sys.argv[1:]
-    count = int(count)
-    depth = int(depth)
-    asyncio.run(main(output_path, count, depth, flag))
+    asyncio.run(main(sys.argv))
